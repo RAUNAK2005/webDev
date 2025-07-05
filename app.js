3ca32302af0f187a347af49052fc2b6b643784dd
RAUNAK2005 <bawejaraunak66@gmail.com>
2025-07-05 23:11:12 +0530

    Add error handling

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// View engine setup
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.get('/', (req, res) => {
-  res.render('index');
+  try {
+    res.render('index');
+  } catch (error) {
+    console.error('Error rendering index:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 const PORT = process.env.PORT || 3000;
